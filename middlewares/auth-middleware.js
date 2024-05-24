dfc88a6ca2f98dd1fbe5d50a69ebffd6b48ce4db
Muhammadumar <khilolamoon@gmail.com>
2024-05-24 19:21:36 +0500

    upgrade

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,11 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = async (id) => {
-  return await jwt.sign({id}, process.env.AUTH_SECRET, {expiresIn: "1h"});
+  return await jwt.sign({ id }, process.env.AUTH_SECRET, { expiresIn: "1h" });
 };
+
+const getAuthorization = async(req, res, next);
 
 // Write a middleware function to defend routes
 // your code...
+
+const verifyToken = async (token) => {
+  return await jwt.verify(token, "secret");
+};
 
 module.exports = {
   generateToken,
